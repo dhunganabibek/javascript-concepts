5455e263fe889caf79ba197e60776acab65fdf42
Bibek Dhungana <bibek.dhungana@precisionplanting.com>
2024-01-23 22:03:56 -0600

    baisc symol added

---
+++ date_and_time.js
@@ -0,0 +1,23 @@
+const today = new Date();
+
+console.log(today.toString());
+
+const localToady = new Date(2023, 0, 23, 4, 20, 30 ); // month is zero index
+console.log(localToady);
+
+
+const formattedDate = (date) => {
+    const padZero = (num) =>  num < 10 ? `0${num}` : `${num}`;
+
+    const YYYY = date.getFullYear().toString();
+    const MM = padZero(date.getMonth() + 1)
+    const DD = padZero(date.getDate());
+
+    const hh = padZero(date.getHours());
+    const mm = padZero(date.getMinutes());
+    const ss = padZero(date.getSeconds())
+
+    return `${YYYY} ${MM} ${DD} ${hh} ${mm} ${ss}`;
+}
+
+console.log(formattedDate(new Date()))
---
+++ symbol.js
@@ -0,0 +1,11 @@
+const one = Symbol();
+const two = Symbol();
+const three = Symbol('Test');
+const four = Symbol('Test');
+
+
+console.log(typeof one);
+console.log(three == four);
+console.log(three);
+console.log(three.toString());
+
