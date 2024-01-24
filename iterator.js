7cbc8882d5645cbd4b9efe24f6023313028f13ee
Bibek Dhungana <bibek.dhungana@precisionplanting.com>
2024-01-23 23:01:28 -0600

    array is iterable because it implement iterator protocol

--- iterator.js
+++ iterator.js
@@ -1,23 +1,38 @@
-// Iterator is Object having next method  when called gives {value: T, done: boolean}
+const a = [1,2,3];
+
+const it = a[Symbol.iterator]()
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
 
 
-function range(start = 0, end= Infinity, step = 1){
-    let value = start;
-    return {
-        next() {
-            if (value > end) return {value: undefined, done: true};
-            const result = {value, done: false}
-            value += step;
-            return result;
-        }
-    }
-}
 
-const it = range(1, 10, 2);
 
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
+
+
+
+
+// // Iterator is Object having next method  when called gives {value: T, done: boolean}
+
+
+// function range(start = 0, end= Infinity, step = 1){
+//     let value = start;
+//     return {
+//         next() {
+//             if (value > end) return {value: undefined, done: true};
+//             const result = {value, done: false}
+//             value += step;
+//             return result;
+//         }
+//     }
+// }
+
+// const it = range(1, 10, 2);
+
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+//  console.log(it.next());
