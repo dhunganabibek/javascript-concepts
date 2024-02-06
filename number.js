2b43e707e59d45378b0582c37c61fe4a6d05c38c
Bibek Dhungana <bibek.dhungana@precisionplanting.com>
2024-02-06 17:37:30 -0600

    WIP

---
+++ number.js
@@ -0,0 +1,9 @@
+console.log("number");
+console.log(1_000_0_00);
+
+console.log(1e3);
+
+console.log(1e-3);
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Infinity == -Infinity);
--- primitivetypes.js
+++ primitivetypes.js
@@ -1,1 +1,3 @@
 // string, number, bigint, boolean, undefined, null, symbol
+
+console.log([[10, 20, 30,1 ,5]].sort())
