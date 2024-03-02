5cad378c977dbd584c7b8ea36559e0a78ba1f18c
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-03-02 10:29:57 -0600

    toNumber(), toString() and toBoolean() abstract operation

--- abstract_operation.js
+++ abstract_operation.js
@@ -1,14 +1,25 @@
-const arr = [1, 2, 3, null, undefined, "hello", { a: 1 }, function () {}];
+// const arr = [1, 2, 3, null, undefined, "hello", { a: 1 }, function () {}];
 
-console.log(arr.toString());
+// console.log(arr.toString());
 
-const obj = { a: 1 };
-console.log(obj.toString());
+// const obj = {
+//   a: 1,
+//   toString: function () {
+//     return "hello";
+//   },
+// };
+// console.log(obj.toString());
 
-function hello() {
-  console.log("hello");
-}
+// function hello() {
+//   console.log("hello");
+// }
 
-console.log(hello.toString());
-console.log(hello.valueOf());
-console.log(hello);
+// console.log(hello.toString());
+// console.log(hello.valueOf());
+// console.log(hello);
+
+console.log(Number(undefined));
+
+// falsy value in javascript "" false 0, -0 , null, undefined, NaN
+
+console.log(Boolean([]));
