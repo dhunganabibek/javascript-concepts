2738f077beb71a807b49405f924c12522b84d85d
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-03-05 00:07:18 -0600

    arrow function does not have this, and can not use new to create object

--- prototype.js
+++ prototype.js
@@ -1,1 +1,11 @@
 console.log("Prototype.js");
+
+const Person = (name) => {
+  this.name = name;
+};
+
+const person = new Person("John");
+
+console.log(person.constructor == Person);
+
+console.log(person.__proto__ == Person.prototype);
