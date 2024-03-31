f8bfc715e6c4574b43bf0c6dd26d5ff516dbcbbf
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-03-30 21:17:36 -0500

    working with in operator

--- generator.js
+++ generator.js
@@ -2,4 +2,13 @@ var nums = [80, 9];
 
 console.log(nums.sort((a, b) => a - b));
 
-console.log(typeof null);
+console.log(typeof [1, 2, 3]);
+
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof (() => {}));
+
+console.log(true + "Hello");
+
+const obj = { a: 1, b: 2, c: 3 };
+console.log("d" in obj);
