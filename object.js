833537183feb4b2ef576df313acee783fab1e6be
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-03-31 14:38:06 -0500

    working with object

--- object.js
+++ object.js
@@ -1,6 +1,15 @@
 let point = { x: 0, y: 0 };
 
-const name = "x";
-console.log(point[name]);
-console.log(point.x);
-console.log(point["y"]);
+const result = delete point.x;
+console.log(result);
+// const name = "x";
+// console.log(point[name]);
+// console.log(point.x);
+// console.log(point["y"]);
+
+// const lang = {
+//   name: "JavaScript",
+//   yearReleased: 1995,
+// };
+
+// console.log(lang["na" + "me"]);
