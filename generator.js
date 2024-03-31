b967149aacf807e6377a69c3c1d195d9073c7565
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-03-31 00:09:52 -0500

    WIP

--- generator.js
+++ generator.js
@@ -13,4 +13,12 @@ console.log(true + "Hello");
 const obj = { a: 1, b: 2, c: 3 };
 console.log("d" in obj);
 
-console.log(0 || 4);
+console.log("Hello" || "World");
+console.log(1 + 2);
+console.log("1" + "2");
+console.log("1" - "2");
+
+console.log(typeof (new Number() + new Number()));
+console.log(1.8e307);
+console.log(Number.MAX_VALUE * 2);
+console.log(Number("1.g Hello"));
