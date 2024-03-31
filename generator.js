2eea51b85b7ae305effe600f3a8683b3b4a4f829
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-03-31 01:12:46 -0500

    charCodeAt method

--- generator.js
+++ generator.js
@@ -1,24 +1,2 @@
-var nums = [80, 9];
-
-console.log(nums.sort((a, b) => a - b));
-
-console.log(typeof [1, 2, 3]);
-
-console.log(typeof {});
-console.log(typeof []);
-console.log(typeof (() => {}));
-
-console.log(true + "Hello");
-
-const obj = { a: 1, b: 2, c: 3 };
-console.log("d" in obj);
-
-console.log("Hello" || "World");
-console.log(1 + 2);
-console.log("1" + "2");
-console.log("1" - "2");
-
-console.log(typeof (new Number() + new Number()));
-console.log(1.8e307);
-console.log(Number.MAX_VALUE * 2);
-console.log(Number("1.g Hello"));
+console.log("😀".charCodeAt(1));
+console.log("😀".charCodeAt(0));
