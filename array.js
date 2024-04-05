ce587c63adedf5ce727f274f60466bf1eafd4660
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-04-05 00:57:22 -0500

    concat only do one level flat

--- array.js
+++ array.js
@@ -1,1 +1,7 @@
-console.log("Array");
+console.log(Number.isInteger(5 ** 0.5));
+console.log(`The array is ${[1, 2, 3]}`);
+console.log([1, 2, 3]);
+Array.prototype[Symbol.inspect] = () => "Array";
+
+const a = [1, 2, 3];
+console.log(a.concat(4, 5, [6, [`7`]]));
