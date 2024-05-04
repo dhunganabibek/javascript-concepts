8d934b81bcfa447f8b72175d3927bbde8caba0ce
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-05-04 18:36:35 -0500

    this confusion

--- whattheheckisthis.js
+++ whattheheckisthis.js
@@ -1,3 +1,12 @@
 console.log("What the heck is this?");
-("use strict");
 console.log(this); // node vs browser
+
+// this depend on called site and every function have this except arrow function
+const arrFunc = () => {
+  console.log(this);
+};
+const regularFunc = function () {
+  console.log(this);
+};
+regularFunc();
+arrFunc();
