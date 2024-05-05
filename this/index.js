55945749659ffa4b9852791f44873f27e4075e69
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-05-05 12:57:44 -0500

    this is calculated from call site object

--- this/index.js
+++ this/index.js
@@ -1,1 +1,8 @@
-console.log("Hello from this");
+const workshop = {
+  teacher: "Kyle",
+  ask: function (question) {
+    console.log(this.teacher, question);
+  },
+};
+
+workshop.ask("What is implicit binding?"); // Kyle What is implicit binding?
