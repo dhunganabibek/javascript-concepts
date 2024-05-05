015f88325177ff49b2db842200b2259d24ac89aa
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-05-05 14:00:32 -0500

    WIP

--- this/index.js
+++ this/index.js
@@ -1,12 +1,5 @@
-const workshop = {
-  teacher: "Kyle",
-  ask: function (question) {
-    console.log(this.teacher, question);
-  },
-};
+function test() {
+  console.log("test");
+}
 
-setTimeout(function () {
-  workshop.ask("Still losing this");
-}, 1000);
-
-setTimeout(workshop.ask.bind(workshop), 2000, "Still losing this");
+console.log(test());
