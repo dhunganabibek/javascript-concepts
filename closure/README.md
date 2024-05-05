479b7a87c897de8f2de42eea134829b82bb5dd9d
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-05-04 21:03:52 -0500

    how do setTimer used with closure

---
+++ closure/README.md
@@ -0,0 +1,3 @@
+## closure
+
+When the function access its lexical scope when it is 3called outside that lexical scope.
---
+++ closure/index.js
@@ -0,0 +1,6 @@
+for (var i = 0; i < 3; i++) {
+  let j = i;
+  setTimeout(function () {
+    console.log(j);
+  }, 1000);
+}
